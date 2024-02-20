b97b063719941d5e69f52c4c1885b56250782feb
manasesrivas <manasesnehemiasr@gmail.com>
2024-02-20 10:44:19 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const usuario = () => {
+    let nombreUsuario = document.getElementById('username').value
+    let apellidoUsuario = document.getElementById('apellido').value
+    alert(`hola biemvenido ${nombreUsuario} ${apellidoUsuario}`)
+}
